ce7681505e3c957e8a36f0e44816f2c3f6a50d5f
YesolLee421 <dldpthf421@gmail.com>
2022-10-05 17:39:19 +0900

    add file delete function in create, update post

--- src/main/resources/static/js/showTmpImage.js
+++ src/main/resources/static/js/showTmpImage.js
@@ -1,70 +1,68 @@
+function deleteAttach() {
+	
+	// img 삭제할 부모 태그
+    const parent = document.querySelector("ul#image-container");
+    
+    // 기존파일 id 정보 담긴 태그
+    const input_attachId = document.querySelector("input#ATTF_ID");
+
+    if(input_attachId) { // id값 이미 있으면 deleteAttach 속성 true로 변경
+        const deleteAttach = document.querySelector('#deleteAttach');
+        deleteAttach.value = true;
+    }
+    // 파일 객체 있으면 삭제
+    const input_attachObj = document.querySelector("input#ATTF_OBJ");
+    if(input_attachObj) {
+    	input_attachObj.value == "";
+//    	input_attachObj.files == [];        
+    }
+    parent.innerHTML = "";
+}
+
 function setThumbnail(event) {
+	
+//	const file = event.target.files.item(0);
+//	console.log("===========file.type : " + file.type);
+	
     const reader = new FileReader();
 
     reader.onload = function(event) {
-
-        const img_wrap = document.createElement('li');
-
-        const i = document.createElement('i');
-        i.setAttribute('class', 'fa-solid fa-trash-can btn-delete');
-        i.addEventListener('click', deleteAttach);
-
-        const img = document.createElement("img");
-        img.setAttribute("src", event.target.result);
-
-        img_wrap.appendChild(img);
-        img_wrap.appendChild(i);
-        
-        // img가 들어갈 부모 찾기
+    	
+    	// img를 넣을 부모 태그
         const parent = document.querySelector("ul#image-container");
-        // 기존 이미지 있으면 삭제
+        
+        // 부모 태그 안 기존 이미지 있으면 삭제
         while (parent.firstChild) {
-            console.log("111111 첨부파일 객체 = " + parent.firstChild);
             parent.firstChild.remove()
         }
-        // 부모 안에 이미지 삽입
+        
+        // 자식태그 감싸는 li태그 생성
+        const img_wrap = document.createElement('li');
+                
+        // 이미지 태그 생성 및 src 삽입
+//        if(file.type.includes("images")) {
+//        	alert("====================includes");
+//        }
+        const img = document.createElement("img");
+        img.setAttribute("src", event.target.result);
+        
+        img_wrap.appendChild(img);
+
+
+
+        // 삭제 아이콘 생성 및 클래스, onclick 삽입
+        const icon = document.createElement('i');
+        icon.setAttribute('class', 'fa-solid fa-trash-can btn-delete');
+        icon.addEventListener('click', deleteAttach);
+       
+        img_wrap.appendChild(icon);
+        
+        // 부모 안에 li 태그 삽입
         parent.appendChild(img_wrap);
     };
     reader.readAsDataURL(event.target.files[0]);
 }
 
-function deleteAttach() {
 
-    const parent = document.querySelector("ul#image-container");
-    const attachId = document.querySelector("input#ATTF_ID").value
-
-    console.log("2222222첨부파일 객체 = " + parent.firstChild);
-    console.log("첨부파일 삭제 id = " + attachId);
-
-    if(attachId) { // id값 이미 있으면 파일도 삭제하기
-        const deleteAttach = document.querySelector('#deleteAttach');
-        deleteAttach.value = true;
-        // ajax
-        // httpRequest = new XMLHttpRequest();
-        // httpRequest.open('DELETE', '/attach/' + attachId, true);
-        // // httpRequest.setRequestHeader('Content-Type', 'application/json');
-
-        // // var data = {
-    	// // 	attachId : attachId
-        // // };
-        
-        // httpRequest.send();
-        
-        // httpRequest.onreadystatechange = () => {
-        //     if (httpRequest.readyState === XMLHttpRequest.DONE) {
-        //         if (httpRequest.status === 200) {
-        //             alert("기존파일 삭제 성공");
-        //         } else {
-        //             alert("실패 : " +  httpRequest.status);
-        //             console.log(httpRequest.response);
-        //         }
-        //     }
-        // }
-    }
-    // 화면에서 이미지 삭제
-    // parent.firstChild.remove();
-    parent.innerHTML = "";
-    document.querySelector("input#ATTF_OBJ").value == "";
-}
 
 
