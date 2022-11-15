6c8d8e82873e1e46994fe92a93aab6db59191202
CEO <JW@DESKTOP-9VBJO1B>
2022-11-15 23:54:08 +0900

    myHome(Update_) 멀티파트폼으로 변경

---
+++ src/main/java/com/ticktack/homey/domain/HomeFormFile.java
@@ -0,0 +1,97 @@
+package com.ticktack.homey.domain;
+
+import org.springframework.web.multipart.MultipartFile;
+
+//실제파일을 가지고있는 저장시 사용하는 도메인 
+public class HomeFormFile {
+	
+	//home
+	Long homeid;
+	Long userid;
+	String homename;
+	String homeinst;
+	String homeuse;
+	String homethema;
+	Long attfid;
+	
+	//file
+	MultipartFile attf_obj;
+	
+	//첨부파일 삭제여부
+	boolean deleteAttach = false;
+	
+	public Long getHomeid() {
+		return homeid;
+	}
+	public void setHomeid(Long homeid) {
+		this.homeid = homeid;
+	}
+	public Long getUserid() {
+		return userid;
+	}
+	public void setUserid(Long userid) {
+		this.userid = userid;
+	}
+	public String getHomename() {
+		return homename;
+	}
+	public void setHomename(String homename) {
+		this.homename = homename;
+	}
+	public String getHomeinst() {
+		return homeinst;
+	}
+	public void setHomeinst(String homeinst) {
+		this.homeinst = homeinst;
+	}
+	public String getHomeuse() {
+		return homeuse;
+	}
+	public void setHomeuse(String homeuse) {
+		this.homeuse = homeuse;
+	}
+	public String getHomethema() {
+		return homethema;
+	}
+	public void setHomethema(String homethema) {
+		this.homethema = homethema;
+	}
+	public Long getAttfid() {
+		return attfid;
+	}
+	public void setAttfid(Long attfid) {
+		this.attfid = attfid;
+	}
+
+
+	
+	public MultipartFile getAttf_obj() {
+		return attf_obj;
+	}
+	public void setAttf_obj(MultipartFile attf_obj) {
+		this.attf_obj = attf_obj;
+	}
+	public boolean isDeleteAttach() {
+		return deleteAttach;
+	}
+	public void setDeleteAttach(boolean deleteAttach) {
+		this.deleteAttach = deleteAttach;
+	}
+	
+	
+	//MyHome에서 Home 객체 반환
+	public Home getHomeFromMFHome() {
+		Home home = new Home();
+		
+		home.setHomeid(this.getHomeid());
+		home.setUserid(this.getUserid());
+		home.setHomename(this.getHomename());
+		home.setHomeinst(this.getHomeinst());
+		home.setHomeuse(this.getHomeuse());
+		home.setHomethema(this.getHomethema());
+		home.setAttfid(this.getAttfid());
+		
+		return home;
+	}
+
+}
--- src/main/resources/static/js/myHome.js
+++ src/main/resources/static/js/myHome.js
@@ -7,92 +7,106 @@ window.onload=function(){
     const upload = document.querySelector('.upload');
 
     upload.addEventListener('click', () => realUpload.click());
-    realUpload.addEventListener('change', getProfileImg);    
+    realUpload.addEventListener('change', setThumbnail);    
     
 }
 
 
-/*'/home/' + homeId + '/profile' 는 AttachController에 있음*/
-/*검증 및 이미지 등록*/
-function getProfileImg(e) {
-	const file = e.currentTarget.files;
-	
-	const input_file = document.querySelector('#uploadFile');
-	const homeid_input = document.querySelector('#homeid');
-	const homeId = homeid_input !=null ? homeid_input.value : null;
+//multipartform으로 변경하면서 쓰이는 것
+function setThumbnail(event) {
 
-	var maxSize  = 1048576;
-	
-    [...file].forEach(file => {
-	    if (!file.type.match("image/.*")) {
+    // fileSize 체크
+    const files = event.currentTarget.files;
 
-	    	alert('이미지 파일만 업로드가 가능합니다.');
-	    	return false;
-	    }
-		if(file.size > maxSize){
-			alert('파일 사이즈는 1MB까지 등록 가능합니다.');
-	   		return false;
-		}
-		else{
-			
-				const formData = new FormData();
-				formData.append('file', input_file.files[0]);
-			
-				fetch('/homes/' + homeId + '/profile', {
-					method : 'POST',
-					body : formData
-				})
-				.then((response) => response.json())
-				.then((attach) => {
-					alert("프로필 변경 성공" + attach.attf_REALNM);
-					displayProfile(attach);
-				})
-				.catch((error) => {
-					console.error('==============error: ',  error);
-				});
-			
-			
-		}
-	});
+    if(!fileSizeValidation(files)) {
+        console.log("fileSize fail");
+        event.target.value = "";
+        return;
+    }
+    console.log("fileSize pass");
+    
+    //img인지 체크
+    if(!imgValidation(files)){
+    	console.log("not img");
+    	event.target.value ="";
+    	return;
+    }
+    console.log("img pass");
 	
-} 
+    const reader = new FileReader();
+    reader.onload = function(event) {
+    	
+    	// img를 넣을 부모 태그
+        const parent = document.querySelector(".imgbox"); 
+        
+        // 부모 태그 안 기존 이미지 있으면 삭제 //이미지 박스 안 이미지를 아예 삭제하고 새로운 이미지를 넣어줘야한다.
+        while (parent.innerHTML!='') {
+            parent.innerHTML="";
+        }
+        
+        //새로운 이미지 자리 만들기
+        const img = document.createElement("img");
+        //이미지 src 넣어주기
+        img.setAttribute("src", event.target.result);
 
-//파일 서버이름으로 url 생성해서 이미지 src에 넣어주는 메소드
-function displayProfile (attach) {
-	const img = document.querySelector('.upload');
-	const attfId = document.getElementById('attf_id');
-	console.log(attach);
-	console.log(attach.attf_ID);
-	img.setAttribute('src', "/images/" + attach.attf_SERNM);
-	attfId.value = attach.attf_ID;
+        //원 안에 이미지 넣기
+        parent.appendChild(img);
+    };
+    reader.readAsDataURL(event.target.files[0]); //얘는 꼭 필요
 }
 
-// 이미지 리셋하기
+//파일 사이즈 검증
+function fileSizeValidation(files) {
+	var maxSize  = 1048576;
+    if([...files][0].size > maxSize) {
+        alert('파일 사이즈는 1MB까지 등록 가능합니다.');
+        return false;
+    }
+	return true;
+}
+
+//이미지인지 검증
+function imgValidation(files){	
+
+    if (![...files][0].type.match("image/.*")) {
+    	alert('이미지 파일만 업로드가 가능합니다.');
+		return false;
+    }
+	
+	return true;
+}
+
+
+
+
+//이미지 리셋하기
 function imgReset(e){
 	if(confirm("이미지를 리셋하시겠어요?")){
 		const attfId = document.getElementById('attf_id').value;
-		const homeId = document.getElementById('homeid').value;
+		//const homeId = document.getElementById('homeid').value;
 		
-		fetch('/homes/' + homeId + '/profileReset/' + attfId, {
-			method : 'POST'
-		})
-		.then(function(response){
-			response.text().then(function(result){
-				if(Boolean(result)){
-					alert("리셋 성공!");
-					const img = document.querySelector('.upload');
-					img.setAttribute('src', "/img/defaultHome4.png");
-					document.getElementById('attf_id').value = "";
-				}
-			})
-		})
-		// https://csdrive.tistory.com/22
+		alert("리셋 성공!");
 		
+		// img를 넣을 부모 태그
+        const parent = document.querySelector(".imgbox"); 
+        
+        // 부모 태그 안 기존 이미지 있으면 삭제 //이미지 박스 안 이미지를 아예 삭제하고 새로운 이미지를 넣어줘야한다.
+        while (parent.innerHTML!='') {
+            parent.innerHTML="";
+        }
+
+		const img = document.createElement("img");
+		img.setAttribute('src', "/img/defaultHome.png");
+		document.getElementById('attf_id').value = "";
+		parent.appendChild(img);
+
+		reader.readAsDataURL(event.target.files[0]); //바이너리 파일을 읽어들일때 사용
 	}else {
 		alert("이미지 리셋 실패");
 		return;
 	}
 }
+
 
 
 function gowith(){
