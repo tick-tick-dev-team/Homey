64daed9d0b82c3319da59d8f21ecd836e66f9908
yesollee <dldpthf421@gmail.com>
2022-12-20 19:05:33 +0900

    post delete error, home list link error solved

--- src/main/resources/static/js/selectHome.js
+++ src/main/resources/static/js/selectHome.js
@@ -1,12 +1,14 @@
-window.onload = function(){
-	document.getElementById("btnDelete").onclick = function() {
-		swal({
-			text : "정말 삭제하시겠습니까?",
-			button: true,
-		})
-		.then((willDelete)=> {
-			if(willDelete) document.getElementById("removeForm").submit();	
-			return false;
-		});		
-	}
+function deletePost(postId) {
+	console.log('js');
+	swal({
+		text : "정말 삭제하시겠습니까?",
+		button: true,
+	})
+	.then((willDelete)=> {
+		if(willDelete) {
+			var formId = "removeForm" + postId;
+			document.getElementById(formId).submit();	
+		}
+		return false;
+	});		
 }
